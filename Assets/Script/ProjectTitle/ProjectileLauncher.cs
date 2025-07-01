3362f2fd4c1f4bcf965fcb48cc860ff892b7725c
5kyR43305 <5kyr43305@gmail.com>
2025-07-01 20:34:54 +0900

    StartProject

---
+++ Assets/Script/ProjectTitle/ProjectileLauncher.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class ProjectTitleLauncher : MonoBehaviour
+{
+    public Transform launcherPoint;
+    public GameObject projectile;
+    public float launchSpeed = 10f;
+
+    [Header("****Trajectory Display****")]
+    public LineRenderer lineRenderer;
+    public int linePoints = 100;
+    public float timeIntervallnPoints = 0.01f;
+
+
+    void Update()
+    {
+        if (lineRenderer != null)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                DrawTrajectory();
+                lineRenderer.enabled = true;
+            }
+            else
+                lineRenderer.enabled = false;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            var _projectile = Instantiate(projectile, launcherPoint.position, launcherPoint.rotation);
+            _projectile.GetComponent<Rigidbody>().linearVelocity = launchSpeed * launcherPoint.up;
+        }
+    }
+    void DrawTrajectory()
+    {
+        Vector3 origin = launcherPoint.position;
+        Vector3 startVelocity = launchSpeed * launcherPoint.up;
+        lineRenderer.positionCount = linePoints;
+        float time = 0;
+
+        for (int i = 0; i < linePoints; i++)
+        {
+            var x = (startVelocity.x * time) + (Physics.gravity.x / 2 * time * time);
+            var y = (startVelocity.y * time) + (Physics.gravity.y / 2 * time * time);
+            Vector3 point = new Vector3(x, y, 0);
+
+            lineRenderer.SetPosition(i, origin + point);
+            time += timeIntervallnPoints;
+        }
+
+
+
+    }
+}
